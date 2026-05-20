b860634add8cbd44d68706343008d6c075855a32
AnciaodaPonte <dupont.helder@gmail.com>
2026-05-20 16:05:46 -0300

    Update Move.java
    fix

--- src/main/java/moves/Move.java
+++ src/main/java/moves/Move.java
@@ -3,20 +3,11 @@ package moves;
 public class Move {
 
     private String name;
-
     private MoveType type;
-
     private MoveCategory category;
-
     private int power;
 
-    public Move(
-            String name,
-            MoveType type,
-            MoveCategory category,
-            int power
-    ) {
-
+    public Move(String name, MoveType type, MoveCategory category, int power) {
         this.name = name;
         this.type = type;
         this.category = category;
@@ -26,15 +17,12 @@ public class Move {
     public String getName() {
         return name;
     }
-
     public MoveType getType() {
         return type;
     }
-
     public MoveCategory getCategory() {
         return category;
     }
-
     public int getPower() {
         return power;
     }
